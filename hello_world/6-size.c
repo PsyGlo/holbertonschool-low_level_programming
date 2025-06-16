3fe5f6236f75a05eb1db3f104dcc82a93a688c7c
Gloria <PsyGlo>
2025-06-16 15:12:40 +0000

    Fix syntax error in 6-size.c by removing extra braces

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(char));
+	printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", (unsigned int)8);
+	printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(float));
+	return (0);
+}
