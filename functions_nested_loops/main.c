2cc3af2e22fbc0a552977b7671c6af4d812d38f3
Gloria <PsyGlo>
2025-06-20 16:58:42 +0000

    wrote a function that adds two integers and returns the result.

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,8 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-    times_table();
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n); /* Should print: 98 */
+
+    n = add(-5, 15);
+    printf("%d\n", n); /* Should print: 10 */
+
+    n = add(0, 0);
+    printf("%d\n", n); /* Should print: 0 */
+
     return (0);
 }
 
