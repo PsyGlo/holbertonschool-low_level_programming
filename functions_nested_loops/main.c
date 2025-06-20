053b56b8e9a9454d0b9d52eae3fb8d5fdcaf4486
Gloria <PsyGlo>
2025-06-20 15:48:20 +0000

    created  a function that checks for lowercase character.

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,8 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-    print_alphabet_x10();
+    int r;
+
+    r = _islower('H');
+    printf("%d\n", r); /* Should print 0 */
+
+    r = _islower('o');
+    printf("%d\n", r); /* Should print 1 */
+
+    r = _islower(108);
+    printf("%d\n", r); /* Should print 1 */
+
     return (0);
 }
 
