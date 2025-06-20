54b2c525b49db9f1372667a582fcc79fe98af0fa
Gloria <PsyGlo>
2025-06-20 11:20:23 +0000

    k

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
 int main (void)
 {
-	putchar('_');	/* prints _ */
-	putchar('p');   /* prints p */
-	putchar('u');   /* prints u */
-	putchar('t');   /* prints t */
-	putchar('c');   /* prints c */
- 	putchar('h');   /* prints h */
- 	putchar('a');   /* prints a */
- 	putchar('r');   /* prints r */
- 	putchar('\n');   /* prints a new line */
+	_putchar('_');	/* prints _ */
+	_putchar('p');   /* prints p */
+	_putchar('u');   /* prints u */
+	_putchar('t');   /* prints t */
+	_putchar('c');   /* prints c */
+ 	_putchar('h');   /* prints h */
+ 	_putchar('a');   /* prints a */
+ 	_putchar('r');   /* prints r */
+ 	_putchar('\n');   /* prints a new line */
 	return 0;
 }
