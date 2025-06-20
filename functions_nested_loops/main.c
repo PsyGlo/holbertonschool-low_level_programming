76c794f86ca3b751d33d57c8b58a93b9db3319ce
Gloria <PsyGlo>
2025-06-20 16:36:31 +0000

    created a function that computes the absolute value of an integer.

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -5,25 +5,17 @@ int main(void)
 {
     int r;
 
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    printf("%d\n", r); /* Should print: +, 1 */
+    r = _abs(98);
+    printf("%d\n", r); /* Should print: 98 */
 
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    printf("%d\n", r); /* Should print: 0, 0 */
+    r = _abs(0);
+    printf("%d\n", r); /* Should print: 0 */
 
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    printf("%d\n", r); /* Should print: -, -1 */
+    r = _abs(-1);
+    printf("%d\n", r); /* Should print: 1 */
 
-    r = print_sign(-523);
-    _putchar(',');
-    _putchar(' ');
-    printf("%d\n", r); /* Should print: -, -1 */
+    r = _abs(-523);
+    printf("%d\n", r); /* Should print: 523 */
 
     return (0);
 }
