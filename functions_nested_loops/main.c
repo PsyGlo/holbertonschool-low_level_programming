0cd4c235dcf1741dc8384e56b95e15f97ea75ecf
Gloria <PsyGlo>
2025-06-20 16:48:24 +0000

    created  a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,25 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 int main(void)
 {
-    int r;
-
-    r = print_last_digit(98);
-    _putchar(',');
-    _putchar(' ');
-    printf("%d\n", r); /* Should print: 8, 8 */
-
-    r = print_last_digit(0);
-    _putchar(',');
-    _putchar(' ');
-    printf("%d\n", r); /* Should print: 0, 0 */
-
-    r = print_last_digit(-1024);
-    _putchar(',');
-    _putchar(' ');
-    printf("%d\n", r); /* Should print: 4, 4 */
-
+    jack_bauer();
     return (0);
 }
 
