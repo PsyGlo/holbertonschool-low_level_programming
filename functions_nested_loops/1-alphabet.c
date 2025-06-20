84c9e52930e49b51c06f22b3e842f25b80c831f0
Gloria <PsyGlo>
2025-06-20 12:51:59 +0000

    a

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: void
+ */
+#include "main.h"
+
+void print_alphabet(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
+    }
+    _putchar('\n');
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+int _putchar(char c); /* Writes a character to stdout */
+void print_alphabet(void); /* Prints the alphabet in lowercase */
 
 #endif /* MAIN_H */
 
