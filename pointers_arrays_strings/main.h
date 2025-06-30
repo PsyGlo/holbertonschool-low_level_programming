af8882121b3f70f88489fd04d1c8729b0131694b
Gloria <PsyGlo>
2025-06-30 15:36:35 +0000

    wrote a function that searches a string for any of a set of bytes.

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
 
 #endif /* MAIN_H */
 
