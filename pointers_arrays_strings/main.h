e4ee3393b0b61972c7183cd92a60912bee3a31c2
Gloria <PsyGlo>
2025-07-02 08:08:50 +0000

    updated 1-memcpy again

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strstr(char *haystack, char *needle);
-int _putchar(char c);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size)i;
+char *_memset(char *s, char b, unsigned int n);
 
 #endif /* MAIN_H */
 
