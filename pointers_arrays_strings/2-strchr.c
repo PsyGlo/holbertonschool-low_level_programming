c71ef828b73b50408a4bc72429e8b5756067f1fb
Gloria <PsyGlo>
2025-07-02 08:47:43 +0000

    updated 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -6,21 +6,21 @@
  * @s: string to search
  * @c: character to find
  *
+ * Description: This function returns a pointer to the first occurrence
+ * of the character c in the string s, or NULL if the character is not found.
+ *
  * Return: pointer to first occurrence of `c` in `s`, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return (s);
-        s++;
-    }
-    
-    /* Check for terminating null byte if 'c' is '\0' */
-    if (c == '\0')
-        return (s);
-    
-    return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	/* Check for terminating null byte if 'c' is '\0' */
+	if (c == '\0')
+		return (s);
+	return (NULL);
 }
-
